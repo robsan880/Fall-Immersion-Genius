f54938fd76ab8a30186f5b67c056a92ad4ede2cb
robsan880 <robsan.wedajo@hgs.hiddengeniusproject.org>
2024-10-02 17:43:04 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,15 @@
-function darkMode() {
-// Your code here.
+function darkMode () {
+    //Const-key: word in Java Script that forever assigns a variable that cannot change
+    const body = document.body;
+    //If the button is clicked is curently white, the screen, the screen should turn black and vice-versa
+    if (body.style.backgroundColor === 'white'){
+        //the screen should turn black
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    }
+
+    else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    }
 }
