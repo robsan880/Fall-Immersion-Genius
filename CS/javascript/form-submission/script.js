68bdfeb65d9ab80262910aacfe45b41fe4459dbb
robsan880 <robsan.wedajo@hgs.hiddengeniusproject.org>
2024-10-12 13:07:00 -0700

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,18 @@
-function formSubmission() {
-    // Your code here.
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('myForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
+
+
+        const name = document.getElementById("name");
+        const email = document.getElementById("email");
+        const message = document.getElementById("message");
+
+        if (name && email && message) {
+            alert(`Form submitted successfully!\nName: ${name}}\nEmail: ${email}\nMessage: ${message}`);
+        } else {
+            alert('Please fill all the fields');
+        }
+    });
+});
