d35cb8d971b17b9bddb5b2e132044019bc37c51b
robsan880 <robsan.wedajo@hgs.hiddengeniusproject.org>
2024-12-05 16:25:31 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,9 @@
 # instruction will below. 
+# Import Flask from the flask libary
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello World"
+if __name__ == '__main__':
+    app.run(debug = True)
